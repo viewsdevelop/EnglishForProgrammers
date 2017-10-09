6c79bb1e2abfa4edcc02c44ee01d74438499150f
Louis Medina <viewsdevelop@gmail.com>
2017-10-09 12:51:30 +0200

    Initial Commit

---
+++ Exercises/VocabularyAndSyntax/dictionary.rb
@@ -0,0 +1,40 @@
+# Define a Hash of several terms.
+
+nouns = {
+  "vez" => "time",
+  "año" => "year",
+  "tiempo" => "time",
+  "día" => "day",
+  "cosa" => "thing"
+}
+
+# Define a method to neatly compare Term with Guess
+
+def test(nouns, term, guess)
+  if guess == nouns[term]
+    puts "Correct!"
+  else
+    puts "Incorrect! Guess again."
+  end
+end
+
+# Select one random Spanish Term
+
+term = nouns.keys.sample
+
+# Prompt the User for an equivalent English term.
+
+puts "What is the English equivalent of #{term}?"
+
+guess = gets.chomp.downcase
+
+# Begin Loop to Continue Prompting User for Correct Answer
+
+until nouns[term] == guess
+  test(nouns, term, guess)
+  guess = gets.chomp.downcase
+end
+
+# Final Message to User
+
+puts "Great job! You now know the meaning of the Spanish word '#{term}'."
